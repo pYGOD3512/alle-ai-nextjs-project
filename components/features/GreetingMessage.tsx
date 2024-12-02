d81e0b766b8350b03fd0f18ef1c93e966b78db88
scifaclint <scifaclint@gmail.com>
2024-12-02 00:43:20 +0000

    greeting component

---
+++ components/features/GreetingMessage.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+
+interface option {
+  label: String;
+}
+interface GreetingMessageProp {
+  username: string;
+  options: option[];
+  handlePressed: (option: option) => void;
+}
+
+const GreetingMessage = ({
+  username,
+  options,
+  handlePressed,
+}: GreetingMessageProp) => {
+  return (
+    <div className="flex flex-col items-center justify-center  mt-5 ">
+      <div className="w-full max-w-md">
+        <div className="flex flex-col text-center mb-6">
+          <h2 className="text-3xl font-bold mb-2 break-words">
+            Hi there, Happy late night! {username} 👋
+          </h2>
+          <p className="text-lg text-gray-500">
+            What would you like to do today?
+          </p>
+        </div>
+        <div className="space-y-4">
+          {options.map((option, index) => (
+            <div
+              key={index}
+              className="bg-white  border hover:bg-secondary rounded-2xl p-2 cursor-pointer "
+              onClick={() => handlePressed(option)}
+            >
+              <p className="text-gray-800 text-sm font-medium">
+                {option.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GreetingMessage;
