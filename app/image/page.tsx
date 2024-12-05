d00b0f9a9e340f5e9cdfd8e6e067283c88463b8c
Alle-AI <dev@alle-ai.com>
2024-12-05 13:51:32 +0000

    manual merge with scifaclint_m

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSidebarStore } from "@/lib/constants";
 import { useEffect } from "react";
+
 export default function ImageGenerationPage() {
   const { isOpen } = useSidebarStore();
   const setCurrentPage = useSidebarStore((state) => state.setCurrentPage);
@@ -9,13 +10,14 @@ export default function ImageGenerationPage() {
     setCurrentPage("image");
   }, [setCurrentPage]);
 
+  
   return (
     <div
       className={`flex flex-col h-[calc(100vh-3.5rem)] transition-all duration-300 text-medium text-center justify-center text-5xl ${
         isOpen ? "pl-60" : "pl-16"
       }`}
     >
-      Image Generation{" "}
+
     </div>
   );
 }
