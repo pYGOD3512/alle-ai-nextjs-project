6d4461f72967f0683d68f97474ef98d611783d54
Alle-AI <dev@alle-ai.com>
2025-01-29 15:37:00 +0000

    authentication page

--- app/ai/(main)/ai/image/res/[chatId]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import ImageArea from "@/components/features/image/ImageArea";
-
-const ImageGenerationPage = () => {
-  return <ImageArea />;
-}
-
-export default ImageGenerationPage
